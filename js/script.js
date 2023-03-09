b0d0c1be522e1465919ae708eaa674de18d37867
mbains-source <114851491+mbains-source@users.noreply.github.com>
2023-03-09 01:25:00 -0800

    Update Script.js 1.2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-alert('Hello world');
+let pokemonList = []
 
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
+let Bulbasaur = {
+  type: 'grass'+'poison',
+  height: 7,
+};
+
+let Charizard = {
+  type: 'fire',
+  height:6
+};
+
+let Pikachu = {
+  type: 'electric',
+  height: 4
+};
+
+pokemonList.push("Bulbasaur","Charizard","Pikachu")
+

2f3d6a3c7a678e2f22211ba47e86fa1be0d2bb04
mbains-source <114851491+mbains-source@users.noreply.github.com>
2023-03-11 00:41:50 -0800

    Commit 1.3

--- js/script.js
+++ js/script.js
@@ -17,3 +17,12 @@ let Pikachu = {
 
 pokemonList.push(bulbasaur,charizard,pikachu)
 
+
+for (let i= 0; i < pokemonList.length;i++){
+  if (pokemonList[i].height>= 5  );
+  document.write(pokemonList[i].name+  "(height: + pokemonList[i].height + "m) - "Wow, that is a big pokemon!" + "<br>";
+} else if (pokemonList[i].height >= 1.6 && pokemonList[i].height < 5) {
+  document.write(pokemonList[i].name + " (height: " +pokemonList[i].height + "m) - That is a medium pokemon!" + "m) - That is a medium pokemon!" + "<br>");
+} else {
+document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + "m) - That is a small pokemon!" + "<br>");
+}

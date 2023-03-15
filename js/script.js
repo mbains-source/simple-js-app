d9fbf887a34b09e1a75bda4c6f779ebae5db73ba
mbains-source <114851491+mbains-source@users.noreply.github.com>
2023-03-15 00:25:06 -0700

    foreach commit 1.5 part 1

--- js/script.js
+++ js/script.js
@@ -18,12 +18,14 @@ let Pikachu = {
 pokemonList.push(bulbasaur,charizard,pikachu)
 
 
-for (let i= 0; i < pokemonList.length;i++){
-  if (pokemonList[i].height>= 5  );
-  document.write(pokemonList[i].name+  "(height: + pokemonList[i].height + "m) - "Wow, that is a big pokemon!" + "<br>";
-} else if (pokemonList[i].height >= 1.6 && pokemonList[i].height < 5) {
-  document.write(pokemonList[i].name + " (height: " +pokemonList[i].height + "m) - That is a medium pokemon!" + "m) - That is a medium pokemon!" + "<br>");
-} else {
-document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + "m) - That is a small pokemon!" + "<br>");
-}
-
+//forEach loop
+pokemonList.forEach(function(pokemon) {
+  if (pokemon.height >=5) {
+      document.write(pokemon.name + " (height: " + pokemon.height + "m) - Wow, that is a big Pokemon!!" + "<br>")
+    } else if (pokemon.height >= 1.6 && pokemon.height < 5) {
+      document.write(pokemon.name + " (height: " + pokemon.height + "m) - Wow, that is a medium Pokemon!" + "<br>")
+    } else {
+      document.write(pokemon.name + " (height: " + pokemon.height + "m) - That is a small pokemon!" + "<br>")
+    }
+  });
+  
